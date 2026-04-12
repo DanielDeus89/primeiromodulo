43e64f7aa13c7d26a2f64b459ce82db8d1eee06a
DanielDeus89 <danielsilvadeus@gmail.com>
2026-04-12 18:36:11 -0300

    mensagem

---
+++ js/lesson10.js
@@ -0,0 +1,229 @@
+const lessonTitle = "Lesson 10";
+const currentVideoId = "Txhw5V23wqs";
+
+const timeRanges = [
+  { start: 10, end: 61 }, //0
+  { start: 61, end: 79 }, //0
+  { start: 95, end: 257 }, //0Conversation
+  { start: 267, end: 350 }, //0Fixacão
+  { start: 360, end: 406 }, //0Passe para o Negativo
+  { start: 416, end: 524 }, //0Fixacão
+  { start: 534, end: 578  }, //0Passe para o Afirmativo
+  { start: 588, end: 691 }, //0Fixacão
+  { start: 701, end: 739 }, //0Passe para o Interrogativo
+  { start: 749, end: 827 }, //0Fixacão
+  { start: 837, end: 879 }, //0
+  { start: 889, end: 1179 }, //0Questions
+  { start: 1179, end: 1589 }, //REsponda
+  { start: 1589, end: 1634 }, //0
+  { start: 1674, end: 1894 }, //0
+  { start: 1904, end: 1944 }, //0
+  { start: 1954, end: 1980 }, //0
+  { start: 2052, end: 2296 }, //0https://youtu.be/Txhw5V23wqs?t=2012
+  { start: 2306, end: 2345 }, //0https://youtu.be/Txhw5V23wqs?t=2012
+
+];
+
+const lessonCards = [
+{
+  "title": "Make your own Senteces",
+  "columns": [
+  ]
+},
+{
+  "title": "Speak Right Now",
+  "type": "speak",
+  "columns": [
+    [
+      
+    ]
+  ]
+},
+{
+  "title": "Conversation",
+  "type": "conversation",
+  "columns": [
+    [
+      ["Do you understand Spanish?", " "],
+      ["No, I don't. I don't understand Spanish. I understand only English.", " "],
+      ["", ""],
+
+      ["Do you have an email?", " "],
+      ["Yes, I do. I have an email.", " "],
+      ["", ""],
+
+      ["Do you w an t to go dow ntow n w ith me now?", " "],
+      ["Sorry, but I have to study English w ith my teacher now.", " "],
+      ["", ""],
+
+      ["Do you speak English w ith your brother?", " "],
+      ["Yes, I do. I speak English w ith my brother.", " "]
+    ]
+  ]
+},
+{
+  "title": "Fixacão",
+  "columns": []  
+},
+{
+  "title": "Passe para o Interrogativo", 
+  "columns": []  
+},
+{
+  "title": "Fixacão",
+  "columns": []  
+},
+{
+  "title": "Passe para o Afirmativo", 
+  "columns": []  
+},
+{
+  "title": "Fixacão",
+  "columns": []  
+},
+{
+  "title": "Passe para o Negativo ",
+  "columns": []  
+},
+{
+  "title": "Fixacão",
+  "columns": []  
+},
+{
+  "title": "Passe para o Interrogativo", 
+  "columns": []  
+},
+{
+  "title": "Questions",
+  "columns": [
+    [
+      ["Do you understand Spanish?", ""],
+      ["Do you speak English at home?", ""],
+      ["Do you have a big car?", ""],
+      ["Do you want to study French with me?", ""],
+      ["Do you want to go to the movies today?", ""],
+      ["Do you drink coffee and milk for breakfast?", ""],
+      ["Do you work with your parents?", ""],
+      ["Do you go to church every week?", ""],
+      ["Where do you want to go tomorrow?", ""],
+      ["Where do you like to go on weekends?", ""],
+      ["What do you like to eat?", ""],
+      ["Do you have time to study every day?", ""]
+    ]
+  ]
+},
+{
+  "title": "Responda",
+  "type": "listening",
+  "segments": [
+   
+  ]
+},
+{
+  "title": "Listening",
+  "type": "listening",
+  "segments": [
+    {
+      "start": 1597,
+      "end": 1601
+    },
+    {
+      "start": 1601,
+      "end": 1607
+    },
+    {
+      "start": 1607,
+      "end": 1615
+    },
+    {
+      "start": 1615,
+      "end": 1623
+    },
+    {
+      "start": 1623,
+      "end": 1634
+    }
+  ]
+},
+{
+  "title": "Responda",
+  "type": "listening",
+  "segments": [
+   
+  ]
+},
+{
+  "title": "Listening",
+  "type": "listening",
+  "segments": [
+    {
+      "start": 1904,
+      "end": 1911
+    },
+    {
+      "start": 1911,
+      "end": 1917
+    },
+    {
+      "start": 1917,
+      "end": 1925
+    },
+    {
+      "start": 1925,
+      "end": 1935
+    },
+    {
+      "start": 1935,
+      "end": 1944
+    }
+  ]
+},
+{
+  "title": "Culture - Labor Day",
+  "columns": [
+    [
+      ["Labor Day is celebrated", "O dia do trabalho é celebrado", 1962, 1965],
+      ["on the first Monday in September.", "na primeira segunda-feira de setembro.", 1965, 1968],
+      ["It is a holiday in honor of labor.", "É um feriado em homenagem ao trabalho.", 1967, 1970],
+      ["Most businesses close,", "A maioria das empresas fecham,", 1970, 1973],
+      ["and there are activities", "e há atividades", 1972, 1974],
+      ["such as speeches,", "como discursos,", 1974, 1976],
+      ["sports events, parades,", "eventos esportivos, paradas,", 1975, 1978],
+      ["and picnics.", "e piqueniques.", 1978, 1980]
+    ]
+  ]
+},
+{
+  "title": "Grammar 2",
+  "columns": [
+    [
+    ]
+  ]
+},
+{
+  "title": "Listening",
+  "type": "listening",
+  "segments": [
+    {
+      "start": 2311,
+      "end": 2318
+    },
+    {
+      "start": 2318,
+      "end": 2323
+    },
+    {
+      "start": 2323,
+      "end": 2329
+    },
+    {
+      "start": 2329,
+      "end": 2336
+    },
+    {
+      "start": 2336,
+      "end": 2345
+    }
+  ]
+},
+];
